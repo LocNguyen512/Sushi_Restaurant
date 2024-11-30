3e1d9ca7b2d73e117799ef6793b06cdb19602826
LocNguyen512 <tanlocnguyen0512@gmail.com>
2024-11-30 22:28:18 +0700

    Create db and import data

---
+++ CreateDB_ImportData/ImportData.sql
@@ -0,0 +1,262 @@
+﻿USE [SUSUSHISHI]
+--------------------------------------------  IMPORT DATA  -------------------------------------------- 
+-- ACCOUNT -- 
+/*
+BULK INSERT ACCOUNT
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\account_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+*/
+-- AREA -- 
+BULK INSERT AREA
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\area_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+
+-- BRANCH_RATING --
+BULK INSERT BRANCH_RATING
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\branch_rating_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- CARD_TYPE --
+insert into CARD_TYPE (TYPE, DISCOUNT_AMOUNT) values ('The thanh vien', 100000);
+insert into CARD_TYPE (TYPE, DISCOUNT_AMOUNT) values ('The silver', 200000);
+insert into CARD_TYPE (TYPE, DISCOUNT_AMOUNT) values ('The gold', 500000);
+GO
+-- CUSTOMER --
+BULK INSERT CUSTOMER
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\customer_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+
+GO
+-- DELIVERY_ORDER --
+BULK INSERT DELIVERY_ORDER
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\dorder_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- DEPARTMENT -- 
+BULK INSERT DEPARTMENT
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\department_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+
+GO
+-- MENU_CATEGORY --
+INSERT INTO [dbo].[MENU_CATEGORY] ([CATEGORY_ID], [CATEGORY_NAME])
+VALUES 
+('C001', N'Khai vị'),
+('C002', N'Sashimi combo'),
+('C003', N'Nigiri'),
+('C004', N'Tempura'),
+('C005', N'Udon'),
+('C006', N'Lẩu'),
+('C007', N'Lunch set'),
+('C008', N'Các món nước');
+GO
+-- DISH --
+INSERT INTO [dbo].[DISH] ([DISH_ID], [DISH_NAME], [DISH_PRICE], [CATEGORY_ID])
+VALUES
+-- Khai vị
+('0001', N'Trứng hấp', 50000, 'C001'),
+('0002', N'Súp miso', 45000, 'C001'),
+('0003', N'Súp chay', 60000, 'C001'),
+('0004', N'Trứng cá hồi', 70000, 'C001'),
+
+-- Sashimi combo
+('0005', N'Sashimi tổng hợp', 150000, 'C002'),
+('0006', N'Sashimi cá hồi đặc biệt', 180000, 'C002'),
+('0007', N'Sashimi cá ngừ', 120000, 'C002'),
+
+-- Nigiri
+('0008', N'Cá ngừ (2 miếng)', 80000, 'C003'),
+('0009', N'Cá hồi (2 miếng)', 90000, 'C003'),
+('0010', N'Lươn cá hồi nướng sốt (2 miếng)', 100000, 'C003'),
+
+-- Tempura
+('0011', N'Tempura tôm', 120000, 'C004'),
+('0012', N'Tempura rau', 70000, 'C004'),
+
+-- Udon
+('0013', N'Udon xào', 95000, 'C005'),
+('0014', N'Udon canh', 110000, 'C005'),
+
+-- Hotpot
+('0015', N'Lẩu hải sản', 250000, 'C006'),
+('0016', N'Lẩu thịt bò', 230000, 'C006'),
+
+-- Lunch set
+('0017', N'Lunch set 1', 180000, 'C007'),
+('0018', N'Lunch set 2', 200000, 'C007'),
+
+-- Các món nước
+('0019', N'Nước cam', 35000, 'C008'),
+('0020', N'Nước trà xanh', 30000, 'C008'),
+('0021', N'Sinh tố dưa hấu', 40000, 'C008');
+
+GO
+-- DISH_AVAILABLE --
+BULK INSERT DISH_AVAILABLE
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\dish_available_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- EMPLOYEE --
+BULK INSERT EMPLOYEE
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\employee_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- INVOICE --
+BULK INSERT INVOICE
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\invoice_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- MEMBERSHIP_CARD --
+BULK INSERT MEMBERSHIP_CARD
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\membership_card_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+
+GO
+-- OFFLINE_ORDER --
+BULK INSERT OFFLINE_ORDER
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\oforder_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- ONLINE_ACCESS_HISTORY --
+BULK INSERT ONLINE_ACCESS_HISTORY
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\online_access_history_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- ONLINE_ORDER --
+BULK INSERT ONLINE_ORDER
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\oorder_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- ORDER --
+BULK INSERT dbo.[ORDER]
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\order_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- ORDER_DISH --
+BULK INSERT ORDER_DISH
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\order_dish_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- RESTAURANT_BRANCH --
+BULK INSERT RESTAURANT_BRANCH
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\restaurant_branch_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- TABLE_ --
+BULK INSERT TABLE_
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\table_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
+GO
+-- WORK_HISTORY --
+BULK INSERT WORK_HISTORY
+FROM 'D:\KHTN\NAM3\1st_Semester\CSDLNC\Sushi_Restaurant\SCRIPT_DATA(CSV)\work_history_data.csv'
+WITH
+(
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+	FIRSTROW = 2,
+    CODEPAGE = '65001' -- UTF-8
+);
